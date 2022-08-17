06bac955f8aa5321dbc8cbc11ef0ebd71cd36602
Zeb Sola <zebsola@zebs-mbp.lan>
2022-08-16 20:21:23 -0700

    some js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById('get-weather');
+
+    function getWeather() {
+        const zip = document.getElementById('zip-code').value;
+        console.log(zip);
+    }
+
+    button.addEventListener('click', getWeather);
+
+
     console.log("Hello World!");
 });
