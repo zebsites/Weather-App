c73bc929be49d03696871789abf6f1f97a0361f5
Zeb Sola <zebsola@zebs-mbp.lan>
2022-08-16 20:41:28 -0700

    hi

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     function getWeather() {
         const zip = document.getElementById('zip-code').value;
         console.log(zip);
+        // let coordRequest = new XMLHttpRequest();
+        // const url = `http://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=17abed46c26a1d3232c55db9a7274603`;
+        let request = new XMLHttpRequest();
+        request.open('GET', `http://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=17abed46c26a1d3232c55db9a7274603`);
+        request.send();
+        request.onload = ()=>{
+            console.log(JSON.parse(request.response));
+        }
+
     }
 
     button.addEventListener('click', getWeather);
 
 
-    console.log("Hello World!");
+    // console.log("Hello World!");
 });
