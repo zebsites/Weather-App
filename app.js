dbbdba018913d0d29ce7f5948ff60850c9c95ebb
Zeb Sola <zebsola@zebs-mbp.lan>
2022-08-18 21:40:23 -0700

    hi

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById('get-weather');
+    const output = document.getElementById('show-weather');
 
     function getWeather() {
         const zip = document.getElementById('zip-code').value;
+        const dateBegin = document.getElementById('date-begin').value;
+        const dateEnd = document.getElementById('date-end').value;
+        console.log(dateBegin, dateEnd);
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${zip}/${dateBegin}/${dateEnd}?unitGroup=metric&include=days&key=RRZU5QYJ5KQL4UP4ET4VVTU9M&contentType=json`
         let coordRequest = new XMLHttpRequest();
-        coordRequest.open('GET', `https://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=17abed46c26a1d3232c55db9a7274603`);
+
+        coordRequest.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                const response = JSON.parse(this.responseText);
+                getElements(response);
+            }
+        };
+
+        coordRequest.open('GET', url, true);
         coordRequest.send();
-        coordRequest.onload = ()=>{
-            console.log(JSON.parse(coordRequest.response));
+
+        function getElements(response) {
+
+            for (var item of response.days)
+            {
+                output.innerHTML += `<div class="weather-box"><h3>${item.datetime}:</h3><br><ul><li>High: ${item.tempmax}°C</li><li>Low: ${item.tempmin}°C</li><li>Conditions: ${item.description}</li></ul></div>`;
+            }
+
         }
-
-
-        let weatherRequest = new XMLHttpRequest();
-        weatherRequest.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=17abed46c26a1d3232c55db9a7274603`);
-        weatherRequest.send();
-        // weatherRequest.onload = ()=>{
-        //     console.log(JSON.parse(weatherRequest.response));
-        // }
     }
-
     button.addEventListener('click', getWeather);
 });
